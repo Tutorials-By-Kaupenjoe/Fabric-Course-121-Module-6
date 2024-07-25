79bc0f5c26dd6e960ddd8aac2c91d91434e38b5c
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-07-25 11:30:34 +0200

    Initial commit

---
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseModDataGenerator.java
@@ -0,0 +1,29 @@
+package net.kaupenjoe.mccourse;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.kaupenjoe.mccourse.datagen.*;
+import net.kaupenjoe.mccourse.world.ModConfiguredFeatures;
+import net.kaupenjoe.mccourse.world.ModPlacedFeatures;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+
+public class MCCourseModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableGenerator::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeGenerator::new);
+		pack.addProvider(ModWorldGenerator::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+		registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
+	}
+}
